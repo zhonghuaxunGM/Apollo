a545608cfc1b8bc1f71b44467395283e181f2c4a
Xu <zhenghao.xu>
2021-12-20 17:52:52 +0800

    add multiple steps

--- src/views/step-b-two/index.vue
+++ src/views/step-b-two/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <a-form-model-item>
-    <a-button @click="precheck">precheck</a-button>
-  </a-form-model-item>
+  <div>
+    <a-row :gutter="16" :style="{ width: '100vw' }">
+      <a-col :span="12">
+        <a-form-model-item>
+          <a-button @click="precheck">precheck</a-button>
+        </a-form-model-item>
+      </a-col>
+
+      <a-col :span="12">
+        <p style="white-space: pre-wrap">{{ response }}</p>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
@@ -14,14 +24,19 @@ export default {
       default: () => ({})
     }
   },
-
+  data() {
+    return {
+      // spinning:false,
+      response: {}
+    }
+  },
   methods: {
     precheck() {
       service({
-        url: '/service-t-k8s/v1/env/pre-check',
+        url: '/service-t-k8s/v1/env/precheck',
         method: 'post',
         data: this.form
-      }).then(res => {
+      }).then((res) => {
         this.response = res
       })
     }
--- src/views/step-e-five/index.vue
+++ src/views/step-e-five/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <div>step 3</div>
+  <div>
+    <a-row :gutter="16" :style="{ width: '100vw' }">
+      <a-col :span="12">
+
+      </a-col>
+
+      <a-col :span="12">
+        <p style="white-space: pre-wrap">{{ response }}</p>
+      </a-col>
+    </a-row>
+  </div>
 </template>
+
+<script>
+import service from '../../utils/request'
+
+export default {
+  props: {
+    form: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      response: {}
+    }
+  },
+  methods: {
+  }
+}
+</script>
